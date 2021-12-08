743f0bf2774409198a87da76899989be97b081c5
Al Bouri Saleem <albouri_salim@yahoo.com>
2021-12-08 14:04:17 +0200

    UPdates 3
    Signed-off-by: Al Bouri Saleem <albouri_salim@yahoo.com>

--- bot-skel.py
+++ bot-skel.py
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 import discord      # base discord module
 import code         # code.interact
 import os           # environment variables
